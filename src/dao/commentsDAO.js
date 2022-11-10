e905f13afda4871058925c2c5fd287da4fed1926
archimonde92 <hoanld@theparallel.io>
2022-11-10 07:53:00 +0700

    fix comments DAO

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -123,11 +123,12 @@ export default class CommentsDAO {
     try {
       // TODO Ticket: User Report
       // Return the 20 users who have commented the most on MFlix.
-      const pipeline = []
+      const pipeline = [{ $group: "$email", "count": { $sum: 1 } },
+      { $sort: { count: -1 } }, { $limit: 20 }]
 
       // TODO Ticket: User Report
       // Use a more durable Read Concern here to make sure this data is not stale.
-      const readConcern = comments.readConcern
+      const readConcern = {w:"majortiry"}
 
       const aggregateResult = await comments.aggregate(pipeline, {
         readConcern,
